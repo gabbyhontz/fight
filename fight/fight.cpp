139fa4aa7a3b5191951df198bd1e2d92b199acd3
Gabby <gnh5055@psu.edu>
2023-11-15 16:03:04 -0500

    added other bosses

--- fight/fight.cpp
+++ fight/fight.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
     std::random_device seed;
     Random rnd(seed());
- 
+
     int character;
     cout << "Choose a character: " << endl;
     cout << "1) Wizard" << endl;
@@ -24,14 +24,14 @@ int main() {
     else if (character == 2) {
         adv = Model("Fighter", 400, 40, 65, 0);
     }
-    //Model adv("Wizard", 350, 35, 55, 0);
-    Model boss("Boss", 50, 1, 2, 5);
 
-    std::cout << "THE FIGHT BEGIN!" << '\n';
-    
+    Model boss1("MiniBoss", 50, 1, 2, 5); //mini boss
+
+    std::cout << "You are fighting a Mini Boss!" << '\n';
+
     int advType;
-    int spaCounter = 5;
-    while (!isDead(adv) && !isDead(boss)) {
+    int spaCounter = 50; //counts turns down from 50
+    while (!isDead(adv) && !isDead(boss1)) {
         do {
             std::cout << '\n'
                 << "1 Normal Attack - 2 Special Attack(" << spaCounter
@@ -39,16 +39,101 @@ int main() {
             std::cin >> advType;
         } while (advType < 1 || advType > 2);
 
-        fight(advType, spaCounter, adv, boss, rnd);
+        fight(advType, spaCounter, adv, boss1, rnd);
     }
 
-    if (isDead(boss)) {
-        std::cout << "You defeated the boss! You Win!" << '\n';
+    if (isDead(boss1)) {
+        std::cout << "You defeated the Mini Boss! You Win!" << '\n';
+    }
+    else {
+        std::cout << "You are DEAD!" << '\n';
+    }
+
+    cout << endl;
+    //////////////////////////////////////////////////
+    cout << "skips to a different battle" << endl;
+    //////////////////////////////////////////////////
+    cout << endl;
+
+    Model boss2("BigBoss", 200, 5, 7, 10); //small boss
+
+    std::cout << "You are fighting the Small Boss!" << '\n';
+
+    while (!isDead(adv) && !isDead(boss2)) {
+        do {
+            std::cout << '\n'
+                << "1 Normal Attack - 2 Special Attack(" << spaCounter
+                << ") : ";
+            std::cin >> advType;
+        } while (advType < 1 || advType > 2);
+
+        fight(advType, spaCounter, adv, boss2, rnd);
+    }
+
+    if (isDead(boss2)) {
+        std::cout << "You defeated the Small Boss! You Win!" << '\n';
+    }
+    else {
+        std::cout << "You are DEAD!" << '\n';
+    }
+
+    cout << endl;
+    //////////////////////////////////////////////////
+    cout << "skips to a different battle" << endl;
+    //////////////////////////////////////////////////
+    cout << endl;
+
+    Model boss3("BigBoss", 350, 10, 20, 15); //medium boss
+
+    std::cout << "You are fighting the Medium Boss!" << '\n';
+
+    while (!isDead(adv) && !isDead(boss3)) {
+        do {
+            std::cout << '\n'
+                << "1 Normal Attack - 2 Special Attack(" << spaCounter
+                << ") : ";
+            std::cin >> advType;
+        } while (advType < 1 || advType > 2);
+
+        fight(advType, spaCounter, adv, boss3, rnd);
+    }
+
+    if (isDead(boss3)) {
+        std::cout << "You defeated the Medium Boss! You Win!" << '\n';
+    }
+    else {
+        std::cout << "You are DEAD!" << '\n';
+    }
+
+    cout << endl;
+    //////////////////////////////////////////////////
+    cout << "skips to a different battle" << endl;
+    //////////////////////////////////////////////////
+    cout << endl;
+
+    Model boss4("BigBoss", 400, 20, 30, 35); //large boss
+
+    std::cout << "You are fighting the Large Boss!" << '\n';
+
+    while (!isDead(adv) && !isDead(boss4)) {
+        do {
+            std::cout << '\n'
+                << "1 Normal Attack - 2 Special Attack(" << spaCounter
+                << ") : ";
+            std::cin >> advType;
+        } while (advType < 1 || advType > 2);
+
+        fight(advType, spaCounter, adv, boss4, rnd);
+    }
+
+    if (isDead(boss4)) {
+        std::cout << "You defeated the Large Boss! You Win!" << '\n';
     }
     else {
         std::cout << "You are DEAD!" << '\n';
     }
 
     return 0;
+
 }
 
